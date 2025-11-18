46fddf89399da97b550d78e59f1429b5e21358d0
shuler7 <vitalisobolevggg@gmail.com>
2025-11-18 10:57:37 +0300

    add quick sort, tests for quick sort

--- src/sort.py
+++ src/sort.py
@@ -3,6 +3,46 @@ from functools import cmp_to_key
 from src.validation import validate_number
 
 
+def get_key_from_key_and_cmp(
+    key: Callable[[int], int] | None, cmp: Callable[[int, int], int] | None
+) -> Callable[[int], int]:
+    """
+    Check is cmp function present and return key function from cmp, otherwise check is key function present and return it.
+    If cmp and key functions not present, returns default ascending sort key function
+    Args:
+        key (Callable[[int], int]): sorting key function
+        cmp (Callable[[int, int], int]): sorting cmp function
+    Returns:
+        Callable[[int], int]: sorting key function
+    """
+    # If cmp is present, returns key function from it
+    if cmp is not None:
+        return cmp_to_key(cmp)  # type: ignore
+    else:
+        if (
+            key is None
+        ):  # If key is ommited, returns default ascending sort key function
+            return lambda x: x
+        return key  # If key is present, returns it
+
+
+def get_validated_list_copy(a: list[int]) -> list[int]:
+    """
+    Validate every element of list is number and make copy of list
+    Args:
+        a (list[int]): list
+    Returns:
+        list[int]: shallow copied list of numbers
+    Raises:
+        TypeError: if non number found in list
+    """
+    a_sorted = []
+    for elem in a:
+        validate_number(elem)
+        a_sorted.append(elem)
+    return a_sorted
+
+
 def bubble_sort(
     a: list[int],
     key: Callable[[int], int] | None = None,
@@ -10,33 +50,22 @@ def bubble_sort(
     reverse: bool = False,
 ) -> list[int]:
     """
-    Bubble sort list a for O(n^2)
+    Bubble sort list 'a' for O(n^2)
     If key and cmp both are present, cmp will replace key function
     Args:
-        a (list[int]): list to sort
-        key (Callable[[T], int]): key function used for custom sorting
-        cmp (Callable[[T, T], int]): compare function user for custom sorting
+        a (list[int]): list
+        key (Callable[[T], int]): sorting key function
+        cmp (Callable[[T, T], int]): sorting compare function
         reverse (bool): is sorting reversed
     Returns:
         list[int]: sorted list
     """
 
-    # If key is ommited, use value as it
-    if key is None:
-        key = lambda x: x
-
-    if cmp is not None:
-        key = cmp_to_key(cmp)  # type: ignore
-
-    # Make shallow copy of list and validate numbers
-    a_sorted = []
-    for elem in a:
-        validate_number(elem)
-        a_sorted.append(elem)
-
-    end = len(a) - 1
+    key = get_key_from_key_and_cmp(key=key, cmp=cmp)
+    a_sorted = get_validated_list_copy(a)
 
     try:
+        end = len(a) - 1
         while end > 0:
             for i in range(end):
                 if not reverse:
@@ -52,7 +81,43 @@ def bubble_sort(
     return a_sorted
 
 
-def quick_sort(a: list[int]) -> list[int]: ...
+def quick_sort(
+    a: list[int],
+    key: Callable[[int], int] | None = None,
+    cmp: Callable[[int, int], int] | None = None,
+    reverse: bool = False,
+) -> list[int]:
+    """
+    Quick sort list 'a' for O(n*log n)
+    If key and cmp both are present, cmp will replace key function
+    Args:
+        a (list[int]): list
+        key (Callable[[T], int]): sorting key function
+        cmp (Callable[[T, T], int]): sorting compare function
+        reverse (bool): is sorting reversed
+    Returns:
+        list[int]: sorted list
+    """
+
+    key = get_key_from_key_and_cmp(key=key, cmp=cmp)
+    a_sorted = get_validated_list_copy(a)
+
+    def _quick_sort(_a: list[int]):
+        if len(_a) == 0:
+            return []
+        _less, _pivot, _more = [], _a[-1], []
+        for _el in _a[:-1]:
+            if not reverse:
+                _less.append(_el) if key(_el) < key(_pivot) else _more.append(_el)
+            else:
+                _less.append(_el) if key(_el) > key(_pivot) else _more.append(_el)
+
+        return [*_quick_sort(_less), _pivot, *_quick_sort(_more)]
+
+    try:
+        return _quick_sort(a_sorted)
+    except TypeError:
+        raise TypeError("Wrong method for object of this type")
 
 
 def counting_sort(a: list[int]) -> list[int]: ...
--- tests/test_sort.py
+++ tests/test_sort.py
@@ -1,5 +1,5 @@
 import pytest
-from src.sort import bubble_sort
+from src.sort import bubble_sort, quick_sort
 
 
 @pytest.fixture
@@ -27,40 +27,81 @@ def str_list():
 
 class TestBubbleSort:
 
-    def test_bubble_sort_with_key(self, unsorted_list) -> None:
+    def test_with_key(self, unsorted_list) -> None:
         assert bubble_sort(unsorted_list, key=lambda x: -x) == sorted(
             unsorted_list, key=lambda x: -x
         )
 
-    def test_bubble_sort_with_key_reversed(self, unsorted_list) -> None:
+    def test_with_key_reversed(self, unsorted_list) -> None:
         assert bubble_sort(
             unsorted_list, key=lambda x: -(-(-x)), reverse=True
         ) == sorted(unsorted_list, key=lambda x: -(-(-x)), reverse=True)
 
-    def test_bubble_sort_with_wrong_key(self, unsorted_list) -> None:
+    def test_with_wrong_key(self, unsorted_list) -> None:
         with pytest.raises(TypeError) as exc:
             bubble_sort(unsorted_list, key=lambda x: -len(x))  # type: ignore
 
         assert exc.errisinstance(TypeError)
         assert exc.match("Wrong method for object of this type")
 
-    def test_bubble_sort_with_wrong_list(self, str_list) -> None:
+    def test_with_wrong_list(self, str_list) -> None:
         with pytest.raises(TypeError) as exc:
             bubble_sort(str_list)
 
         assert exc.errisinstance(TypeError)
         assert exc.match("List contains non number objects")
 
-    def test_bubble_sort_with_cmp(self, unsorted_list) -> None:
+    def test_with_cmp(self, unsorted_list) -> None:
         def compare_func(a, b):
             return a - b
 
         assert bubble_sort(unsorted_list, cmp=compare_func) == sorted(unsorted_list)
 
-    def test_bubble_sort_with_key_and_cmp(self, unsorted_list) -> None:
+    def test_with_key_and_cmp(self, unsorted_list) -> None:
         def compare_func(a, b):
             return b - a
 
         assert bubble_sort(unsorted_list, key=lambda x: x, cmp=compare_func) == sorted(
+            unsorted_list, reverse=True
+        )
+
+
+class TestQuickSort:
+
+    def test_with_key(self, unsorted_list) -> None:
+        assert quick_sort(unsorted_list, key=lambda x: -x) == sorted(
+            unsorted_list, key=lambda x: -x
+        )
+
+    def test_with_key_reversed(self, unsorted_list) -> None:
+        assert quick_sort(
+            unsorted_list, key=lambda x: -(-(-x)), reverse=True
+        ) == sorted(unsorted_list, key=lambda x: -(-(-x)), reverse=True)
+
+    def test_with_wrong_key(self, unsorted_list) -> None:
+        with pytest.raises(TypeError) as exc:
+            quick_sort(unsorted_list, key=lambda x: -len(x))  # type: ignore
+
+        assert exc.errisinstance(TypeError)
+        assert exc.match("Wrong method for object of this type")
+
+    def test_with_wrong_list(self, str_list) -> None:
+        with pytest.raises(TypeError) as exc:
+            quick_sort(str_list)
+
+        assert exc.errisinstance(TypeError)
+        assert exc.match("List contains non number objects")
+
+    def test_with_cmp(self, unsorted_list) -> None:
+        def compare_func(a, b):
+            return a - b
+
+        assert quick_sort(unsorted_list, cmp=compare_func) == sorted(unsorted_list)
+
+    def test_with_key_and_cmp(self, unsorted_list) -> None:
+        def compare_func(a, b):
+            return b - a
+
+        assert quick_sort(unsorted_list, key=lambda x: x, cmp=compare_func) == sorted(
             unsorted_list, reverse=True
         )
