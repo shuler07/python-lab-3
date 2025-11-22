8a8bd8ba2031fbd373d0a8669641ce81ac3a10f1
shuler7 <vitalisobolevggg@gmail.com>
2025-11-22 10:59:39 +0300

    add benchmark_sorts, print_formated_benchmark_sorts functions

--- src/benchmarks.py
+++ src/benchmarks.py
@@ -15,16 +15,47 @@ def timeit_once(func: Callable[[Any], Any], *args: Any, **kwargs: Any) -> float:
         Exception: if error occured while executing a function
     """
 
-    try:
-        tstart = datetime.now()
-        func(*args, **kwargs)
-    except Exception as exc:
-        raise exc
-
+    tstart = datetime.now()
+    func(*args, **kwargs)
     time = datetime.now() - tstart
-    return round(time.seconds + time.microseconds // 10**3) / 1000
+
+    return round((time.seconds + time.microseconds / 10**6) * 10**5) / 10**5
 
 
 def benchmark_sorts(
     arrays: dict[str, list], algos: dict[str, Callable]
-) -> dict[str, dict[str, float]]: ...
+) -> dict[str, dict[str, float]]:
+    """
+    Benchmark sorts
+    Args:
+        arrays (dict[str, list]): object with lists to test sortings on, key - list name, value - list
+        algos (dict[str, Callable]): object with sortings to test, key - sorting name, value - sorting function
+    Returns:
+        dict(str, dict(str, float)): object with results, key - sorting name, value - object, where key - list name, value - taken time
+    Raises:
+        Exception: if error occured while executing a sorting function
+    """
+
+    results: dict[str, dict[str, float]] = {}
+
+    for algname, algfunc in algos.items():
+        results[algname] = {}
+        for arrname, arrlist in arrays.items():
+            results[algname][arrname] = timeit_once(algfunc, arrlist)
+
+    return results
+
+
+def print_formated_benchmark_sorts(results: dict[str, dict[str, float]]) -> None:
+    "Print beatiful table with sorting functions benchmarks results"
+    algos = list(results.keys())
+    print(f'|{"List name":^16}|', end="")
+    for alg in algos:
+        print(f"{alg:^16}|", end="")
+    print()
+    for arr in results[algos[0]].keys():
+        print(f"|{arr:^16}|", end="")
+        for alg in algos:
+            res = results[alg][arr]
+            print(f"{f'{res}s':^16}|", end="")
+        print()
